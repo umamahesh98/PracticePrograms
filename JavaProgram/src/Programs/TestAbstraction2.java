a0971ea0b6df5004e9f480c996fb1dbcd1c50dd4
tech <tech@172.20.0.95>
2021-03-03 17:06:19 +0530

    update

--- JavaProgram/src/Programs/TestAbstraction2.java
+++ JavaProgram/src/Programs/TestAbstraction2.java
@@ -1,19 +1,27 @@
 package Programs;
 
-abstract class Bike{  
-	   Bike(){System.out.println("bike is created");}  
-	   abstract void run();  
-	   void changeGear(){System.out.println("gear changed");}  
-	 }  
-	
-	 class Honda extends Bike{  
-	 void run(){System.out.println("running safely..");}  
-	 }  
-	
-	 class TestAbstraction2{  
-	 public static void main(String args[]){  
-	  Bike obj = new Honda();  
-	  obj.run();  
-	  obj.changeGear();  
-	 }  
-	}  
+abstract class Bike {
+	Bike() {
+		System.out.println("bike is created");
+	}
+
+	abstract void run();
+
+	void changeGear() {
+		System.out.println("gear changed");
+	}
+}
+
+class Honda extends Bike {
+	void run() {
+		System.out.println("running safely..");
+	}
+}
+
+class TestAbstraction2 {
+	public static void main(String args[]) {
+		Bike obj = new Honda();
+		obj.run();
+		obj.changeGear();
+	}
+}
